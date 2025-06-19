7174cb5f7ff0835cf618b122920b2c178f55ce61
ParfaitCMD <devparfaitdev20@gmail.com>
2025-06-18 22:59:04 -0300

    juiz código controller e repository

--- backend/codeplac/src/main/java/codeplac/codeplac/Controller/JuizCodigoController.java
+++ backend/codeplac/src/main/java/codeplac/codeplac/Controller/JuizCodigoController.java
@@ -1,38 +1,25 @@
 package codeplac.codeplac.Controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import codeplac.codeplac.DTO.ResponsesDTO.JuizCodigoDTO;
 import codeplac.codeplac.Model.JuizCodigoModel;
 import codeplac.codeplac.Repository.JuizCodigoRepository;
 
 @RestController
-@RequestMapping("/juiz")
+@RequestMapping("/juizcodigo")
+@CrossOrigin(origins = "*") // Permitir requisições do frontend
 public class JuizCodigoController {
-@Autowired
+
+    @Autowired
     private JuizCodigoRepository repository;
 
-    @PostMapping("/enviar")
-    public JuizCodigoModel enviarCodigo(@RequestBody JuizCodigoDTO dto) {
-        JuizCodigoModel juiz = new JuizCodigoModel();
-        juiz.setNomeEquipe(dto.getNomeEquipe());
-        juiz.setNumeroCodigo(dto.getNumeroCodigo());
-        juiz.setNomeLider(dto.getNomeLider());
-        juiz.setCodigo(dto.getCodigo());
-
-        return repository.save(juiz);
-    }
-
-    @GetMapping("/listar")
-    public List<JuizCodigoModel> listarTodos() {
-        return repository.findAll();
+    @PostMapping
+    public JuizCodigoModel receberCodigo(@RequestBody JuizCodigoModel dados) {
+        return repository.save(dados);
     }
 }
-
--- backend/codeplac/src/main/java/codeplac/codeplac/Repository/JuizCodigoRepository.java
+++ backend/codeplac/src/main/java/codeplac/codeplac/Repository/JuizCodigoRepository.java
@@ -1,9 +1,8 @@
 package codeplac.codeplac.Repository;
 
-import codeplac.codeplac.Model.JuizCodigoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import codeplac.codeplac.Model.JuizCodigoModel;
+
 public interface JuizCodigoRepository extends JpaRepository<JuizCodigoModel, Long> {
 }
